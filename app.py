dae10d8ff8d4e889ec708f6f530781eeec3a3979
lalith nivas <lalithnivasy@gmail.com>
2025-07-23 12:11:36 +0530

    Add interested properties list to sample data: include various property options for user selection

--- app.py
+++ app.py
@@ -205,6 +205,14 @@ def test():
                 {"id": 1, "name": "Satish Kumar"},
                 {"id": 2, "name": "Anita Sharma"}
             ],
+            "InterstedIn" : [
+                {"id": "Vasudha Township","name":"Vasudha Township"},
+                {"id": "Fortune City","name": "Fortune City"},
+                {"id": "Royal Smart City","name": "Royal Smart City"},
+                {"id": "Vasudha Township","name":"Vasudha Township"},
+                {"id": "Sree Pavitra Avenue","name":"Sree Pavitra Avenue"},
+                {"id": "Nandha Gokulam","name":"Nandha Gokulam"},
+            ],
 
         }
         if key in sample_data:
